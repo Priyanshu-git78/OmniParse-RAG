d41c753aaccf7e53cddbe8dc9154e46b41bd87a6
Pranshu <pranshuharsana@gmail.com>
2026-07-11 03:12:00 +0530

    Stream lit launch

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ st.title("This is RAG Pipeline Build for Demo Purpose")
 
 
 #upload dir and logic 
-UPLOAD_DIR= Path("uploads")
-UPLOAD_DIR.mkdir(exist_ok=True)
-uploaded_file=st.file_uploader("upload your documents for here (optional)")
-if uploaded_file:
-    file_path= UPLOAD_DIR/uploaded_file.name
-    with open(file_path,"wb") as f:
-        f.write(uploaded_file.getbuffer())
-        st.success(f"Saved: {file_path}")
+# UPLOAD_DIR= Path("uploads")
+# UPLOAD_DIR.mkdir(exist_ok=True)
+# uploaded_file=st.file_uploader("upload your documents for here (optional)")
+# if uploaded_file:
+#     file_path= UPLOAD_DIR/uploaded_file.name
+#     with open(file_path,"wb") as f:
+#         f.write(uploaded_file.getbuffer())
+#         st.success(f"Saved: {file_path}")
 
 # uploaded Retrival to 
 
